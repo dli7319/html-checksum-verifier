117c3f58e3ad24dfaf18e47265392980ceab7554
David Li <david@davidl.me>
2025-02-03 00:57:02 +0000

    Remove unused node-forge stuff.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,16 +1,1 @@
 declare module '*.css';
-
-declare module 'node-forge/lib/md5' {
-    import forge from 'node-forge';
-    export default forge.md5;
-}
-
-declare module 'node-forge/lib/sha1' {
-    import forge from 'node-forge';
-    export default forge.sha1;
-}
-
-declare module 'node-forge/lib/sha256' {
-    import forge from 'node-forge';
-    export default forge.sha256;
-}
